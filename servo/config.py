implemented real time normalization during walking + bugfix on telescope during first days of commissioning

servo/config.py
@@ -12,6 +12,8 @@ IRCAM_VIEWER_OUTPUT_TIME = 0.1 #s
 
 IRCAM_BUFFER_SIZE = 100
 IRCAM_LOST_THRESHOLD = CALIBRATION_LASER_WAVELENGTH / 2 * 0.8 # nm (80% of lambda/2 to be safe)
+IRCAM_MAX_FPS_ROI_MODE = 8000
+IRCAM_MAX_FPS_FF_MODE = 800
 
 FRAME_DTYPE = np.float32
 DATA_DTYPE = np.float32
@@ -35,17 +37,23 @@ PIEZO_V_MAX = 9.95
 OPD_TOLERANCE = 5.0  # nm
 PIEZO_MAX_OPD_DIFF = 5000 # nm
 
+
+SERVO_CPU_VIEWER = 1
+SERVO_CPU_DEFAULT = 0
+SERVO_CPU_IRCAM = 2
+
 SERVO_BUFFER_SIZE = 20 # for servo values buffering: servo updates are based on mean of this buffer
 SERVO_NORMALIZE_REC_TIME = 1.0 # s, time to record values for normalization coeffs computation
 SERVO_NORMALIZE_REC_SIZE = 10000 # s, time to record values for normalization coeffs computation
 VIEWER_BUFFER_SIZE = 1000 # for viewer servo values buffering
 SERVO_DEFAULT_NICENESS = 0
 SERVO_MAX_NICENESS = -20
-SERVO_LOW_NICENESS = 10
+SERVO_LOW_NICENESS = 15
 SERVO_OPD_TIMEOUT = 5.0 # s
 SERVO_NONCRITIC_REFRESH_TIME = 1 # s, time to refresh servo config (PID coeffs, etc.)
 
 VIEWER_ELLIPSE_DRAW_BUFFER_SIZE = 100
+
 
 DEFAULT_PROFILE_LEN = MIN_ROI_SHAPE
 DEFAULT_PROFILE_WIDTH = 4
@@ -85,6 +93,7 @@ NEXLINE_EVENTS = (
 TRACKER_EVENTS = (
     'start_recording',
     'stop_recording',
+    'normalize',
     )
 
 # use only this part of the profiles for normalization coeffs computation

servo/core.py
@@ -178,7 +178,7 @@ class SharedData(object):
         self.add_array('Servo.pixels_x', np.zeros(config.FULL_FRAME_SHAPE[0],
                                                   dtype=int), stored=True)
         self.add_array('Servo.pixels_y', np.zeros(config.FULL_FRAME_SHAPE[1],
-                                                  dtype=int), stored=True)
+                                                  dtype=int), stored=True)        
         
         
         self.add_array('Servo.roinorm_min', np.zeros(config.FULL_FRAME_SIZE,
@@ -239,6 +239,7 @@ class SharedData(object):
 
         self.add_value('Tracker.state', float(0), stored=False)
         self.add_value('Tracker.is_recording', False, stored=False)
+        self.add_value('Tracker.is_rt_normalizing', False, stored=False)
         self.add_value('Tracker.frequency', float(np.nan), stored=False)
         frequencies = [int(ifreq) for ifreq in config.TRACKER_STATS_FREQUENCIES]
         for ifreq in frequencies:

servo/ircam.py
@@ -96,7 +96,12 @@ class IRCamera(core.Worker):
         min_fps = self.dev.minFps()
         max_fps = self.dev.maxFps()
         log.info(f"fps range: ({self.dev.minFps()} - {self.dev.maxFps()})")
-        self.dev.setFps(max_fps)
+        if self.roi_mode:
+            target_fps = min(max_fps, config.IRCAM_MAX_FPS_ROI_MODE)
+        else:
+            target_fps = min(max_fps, config.IRCAM_MAX_FPS_FF_MODE)
+            
+        self.dev.setFps(target_fps)
         self.dev.updateConfig()  #Data is sent to the device
         log.info(f"current fps: {self.dev.fps()}")
 
@@ -150,6 +155,9 @@ class ConfigObserver(NITLibrary.NITConfigObserver):
     
     def onNucChanged(self, nuc_str, status ):
         log.debug("onNucChanged(" + nuc_str + ", " + str(status) + ")")
+
+    def onInternalError(self, error_str):
+        log.error("onInternalError(" + error_str + ")")
 
 
 class DataObserver(NITLibrary.NITUserObserver):
@@ -234,7 +242,7 @@ class DataObserver(NITLibrary.NITUserObserver):
         try:
             frame_time = time.perf_counter()
             frame_id = int(frame.id())
-            if frame_id == self.last_id:
+            if frame_id <= self.last_id:
                 return
             else:
                 self.last_id = frame_id
@@ -286,7 +294,7 @@ class DataObserver(NITLibrary.NITUserObserver):
                     self.profile_len = int(self.data['IRCamera.profile_len'][0])
                 if self.profile_len > np.min(self.frame_shape):
                     self.profile_len = int(np.min(self.frame_shape))
-                self.iwid = int(self.data['IRCamera.profile_width'][0])
+                self.iwid = int(self.data['params.PROFILE_WIDTH'][0])
 
                 
             if bool(self.arr_full_output[0]):
@@ -337,7 +345,6 @@ class DataObserver(NITLibrary.NITUserObserver):
                     hprofile, self.hmin, self.hmax, inplace=False)
                 self.arr_vprof_norm[:self.profile_len] = utils.normalize_profile(
                     vprofile, self.vmin, self.vmax, inplace=False)
-
                 
             # Levels (3 positions per axis)
             self.hlevels_ws[:] = batch_normalize_and_levels(

servo/servo.py
@@ -334,7 +334,8 @@ class Servo(core.Worker):
 
         if priority is None:
             log.info(f"Starting worker {WorkerClass.__name__} with default priority")
-            priority={"niceness": config.SERVO_DEFAULT_NICENESS, "cpus": [1]}
+            priority={"niceness": config.SERVO_DEFAULT_NICENESS,
+                      "cpus": config.SERVO_CPU_DEFAULT}
             
         worker = multiprocessing.Process(
             target=worker_process,
@@ -381,7 +382,7 @@ class Servo(core.Worker):
                     if time.time() - timeout_start > 5:
                         log.error("Timeout waiting for IRCamera initialization; starting viewer anyway")
                         break
-            self.start_worker(viewer.Viewer, priority={"niceness": config.SERVO_LOW_NICENESS, "cpus": [0]})
+            self.start_worker(viewer.Viewer, priority={"niceness": config.SERVO_LOW_NICENESS, "cpus": [config.SERVO_CPU_VIEWER]})
 
         
     def _normalize(self, _):
@@ -575,6 +576,7 @@ class Servo(core.Worker):
         move_startt = time.perf_counter()
         refresh_startt = time.perf_counter()
         last_update_time = time.perf_counter()
+        last_normalization_time = time.perf_counter()
 
         step_velocity_adjustment_factor = 1.0 # nexline velocity adjustment factor for each step to compensate for piezo contribution, will be updated at each step end based on the estimated velocity error ratio
 
@@ -659,6 +661,11 @@ class Servo(core.Worker):
                         pid_da2_control.update_config(out_min=da2_min, out_max=da2_max)
 
                         refresh_startt = time.perf_counter()
+
+                    if time.perf_counter() - last_normalization_time > self.data['params.SERVO_WALK_NORMALIZE_TIME'][0]:
+                        self.events['Tracker.normalize'].set() #
+                        last_normalization_time = time.perf_counter()
+
 
 
                     if ((np.abs(opd - final_opd_target) < config.OPD_TOLERANCE)
@@ -896,7 +903,8 @@ class Servo(core.Worker):
         x = self.data['IRCamera.profile_x'][0]
         y = self.data['IRCamera.profile_y'][0]
         self.start_worker(ircam.IRCamera,
-                          priority={"niceness": config.SERVO_MAX_NICENESS, "cpus": [2]},
+                          priority={"niceness": config.SERVO_MAX_NICENESS,
+                                    "cpus": [config.SERVO_CPU_IRCAM]},
                           frame_shape=(w, w),
                           frame_center=(x, y),
                           roi_mode=True)
@@ -910,7 +918,10 @@ class Servo(core.Worker):
         log.info("Switching to full frame mode")
         self.events['IRCamera.stop'].set()
         time.sleep(0.5) # wait for camera to stop
-        self.start_worker(ircam.IRCamera, -20, roi_mode=False)
+        self.start_worker(ircam.IRCamera,
+                          priority={"niceness": config.SERVO_MAX_NICENESS,
+                                    "cpus": [config.SERVO_CPU_IRCAM]},
+                          roi_mode=False)
                 
     
     def _stop(self, _):

servo/tracker.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from . import core
 from . import config
 from . import utils
+from . import faster
 from .fsm import Transition
 
 log = logging.getLogger(__name__)
@@ -101,6 +102,32 @@ class Record():
         log.info(f'Tracker ir cube saved to {ircube_filename}')
         np.save(ircube_normalized_filename, ir_cube_normalized)
         log.info(f'Tracker normalized ir cube saved to {ircube_normalized_filename}')
+
+    def get_normalization_coeffs(self, width):
+
+        if self.position < 100:
+            log.warning(f'Not enough data to compute normalization coefficients: only {self.position} frames recorded')
+            return None, None, None, None
+        
+        ircube = self.ir_images[:self.position].copy()
+        
+        # recompute normalization maps
+        ix, iy = np.array(ircube.shape[1:])//2
+        profile_len = ircube.shape[1]
+        hprofiles = np.empty((ircube.shape[0], ircube.shape[1]), dtype=np.float32)
+        vprofiles = np.empty_like(hprofiles)
+        for iz, iframe in enumerate(ircube):
+            ivprof, ihprof, _ = faster.compute_profiles_local_f32(iframe.astype(np.float32, copy=False).T,
+                                                                  int(iy), int(ix), int(width),
+                                                                  int(profile_len), True)
+            hprofiles[iz, :] = ihprof.copy()
+            vprofiles[iz, :] = ivprof.copy()
+
+        hnorm = utils.get_normalization_coeffs(hprofiles)      
+        vnorm = utils.get_normalization_coeffs(vprofiles)
+        roinorm_min, roinorm_max = utils.get_roi_normalization_coeffs(ircube)
+
+        return hnorm, vnorm, roinorm_min.T, roinorm_max.T
         
         
 
@@ -114,8 +141,10 @@ class Tracker(core.Worker):
                 self.State.RUNNING, action=self._start_recording),
             (self.State.RUNNING, self.Event.STOP_RECORDING): Transition(
                 self.State.RUNNING, action=self._stop_recording),
+            (self.State.RUNNING, self.Event.NORMALIZE): Transition(
+                self.State.RUNNING, action=self._normalize),        
         }
-
+        
         self.frequencies = [int(ifreq) for ifreq in config.TRACKER_STATS_FREQUENCIES]
         if config.TRACKER_FREQUENCY not in self.frequencies:
             self.frequencies.append(config.TRACKER_FREQUENCY)
@@ -247,6 +276,22 @@ class Tracker(core.Worker):
         self.is_recording = False
         self.data['Tracker.is_recording'][0] = False
         self.record.save()
+
+    def _normalize(self, _):
+        log.info('Normalizing tracker data')
+        width = int(self.data['params.PROFILE_WIDTH'][0])
+        
+        hnorm, vnorm, roinorm_min, roinorm_max = self.record.get_normalization_coeffs(width)
+        
+        self.data['Servo.hnorm_min'][:self.profile_len] = hnorm[:,0]
+        self.data['Servo.hnorm_max'][:self.profile_len] = hnorm[:,1]
+        self.data['Servo.vnorm_min'][:self.profile_len] = vnorm[:,0]
+        self.data['Servo.vnorm_max'][:self.profile_len] = vnorm[:,1]
+        self.data['Servo.roinorm_min'][:self.profile_len**2] = roinorm_min.astype(config.FRAME_DTYPE).flatten()
+        self.data['Servo.roinorm_max'][:self.profile_len**2] = roinorm_max.astype(config.FRAME_DTYPE).flatten()
+
+        self._init_normalization_coeffs()
+        
         
     def loop_once(self):
         