real time normalization implemented + bugfix. Ok for observing on telescope

servo/core.py
@@ -173,6 +173,9 @@ class SharedData(object):
         self.add_value('IRCamera.state', float(0), stored=False)
         self.add_value('IRCamera.full_output', float(0), stored=False)
 
+        self.add_value('IRCamera.profile_h_shift', int(0), stored=True)
+        self.add_value('IRCamera.profile_v_shift', int(0), stored=True)
+
         # selected pixels: 0:none, 1:side, 2:center
         self.add_value('Servo.state', float(0), stored=False)
         
@@ -234,13 +237,14 @@ class SharedData(object):
         self.add_value('Tracker.is_recording', False, stored=False)
         self.add_value('Tracker.is_rt_normalizing', False, stored=False)
         self.add_value('Tracker.frequency', float(np.nan), stored=False)
-        frequencies = [int(ifreq) for ifreq in config.TRACKER_STATS_FREQUENCIES]
+        frequencies = [ifreq for ifreq in config.TRACKER_STATS_FREQUENCIES]
         for ifreq in frequencies:
-            self.add_value(f'Tracker.opd_{ifreq}', float(np.nan), stored=False)
-            self.add_value(f'Tracker.tip_{ifreq}', float(np.nan), stored=False)
-            self.add_value(f'Tracker.tilt_{ifreq}', float(np.nan), stored=False)
-            self.add_value(f'Tracker.opd_std_{ifreq}', float(np.nan), stored=False)
-            self.add_value(f'Tracker.velocity_{ifreq}', float(np.nan), stored=False)
+            ifreq_str = f"{int(ifreq)}" if ifreq % 1 == 0 else f"{ifreq:.1f}"
+            self.add_value(f'Tracker.opd_{ifreq_str}', float(np.nan), stored=False)
+            self.add_value(f'Tracker.tip_{ifreq_str}', float(np.nan), stored=False)
+            self.add_value(f'Tracker.tilt_{ifreq_str}', float(np.nan), stored=False)
+            self.add_value(f'Tracker.opd_std_{ifreq_str}', float(np.nan), stored=False)
+            self.add_value(f'Tracker.velocity_{ifreq_str}', float(np.nan), stored=False)
 
         # FIR metrics
         self.add_value('FIR.short.tracking.e_opd', float(0), stored=False)

servo/ircam.py
@@ -237,6 +237,9 @@ class DataObserver(NITLibrary.NITUserObserver):
         self.normalization_coeffs_tag = self._get_normalization_coeffs_tag()
         self.roinorm_min = None
         self.roinorm_max = None
+        self.roinorm_amp = None
+        self.roinorm_mean = None
+        self.roinorm_mask = None
 
         gc.freeze()
         gc.disable()
@@ -276,7 +279,8 @@ class DataObserver(NITLibrary.NITUserObserver):
             frame_data = frame.data()  # native order, no transpose
     
             # --------- T0: viewer copy (and potential resize) ----------                
-            if frame_time - self.last_viewer_out_time > config.IRCAM_VIEWER_OUTPUT_TIME:
+            if ((frame_time - self.last_viewer_out_time > config.IRCAM_VIEWER_OUTPUT_TIME)
+                or (self.roinorm_min is None) or (self.roinorm_max is None)):
                 self.last_viewer_out_time = frame_time
 
                 # Ensure last_frame has correct size 
@@ -300,14 +304,16 @@ class DataObserver(NITLibrary.NITUserObserver):
                     self.ix = self.data['IRCamera.profile_x'][0] - self.frame_position[0]
                     self.iy = self.data['IRCamera.profile_y'][0] - self.frame_position[1]
                     self.profile_len = int(self.data['IRCamera.profile_len'][0])
+                
                 if self.profile_len > np.min(self.frame_shape):
                     self.profile_len = int(np.min(self.frame_shape))
                 self.iwid = int(self.data['params.PROFILE_WIDTH'][0])
 
                 # reload normalization coefficients if changed (check tag)
-                if (np.any(self._get_normalization_coeffs_tag() != self.normalization_coeffs_tag)
-                    or self.roinorm_min is None
-                    or self.roinorm_max is None):
+                if ((np.any(self._get_normalization_coeffs_tag() != self.normalization_coeffs_tag))
+                    or (self.roinorm_min is None)
+                    or (self.roinorm_max is None)
+                    or (self.roinorm_amp is None)):
                     log.warning('Normalization coefficients changed. Reinitializing IR image buffers.')
             
                     self.roinorm_min = np.array(self.data['Servo.roinorm_min'][:self.profile_len**2]).reshape(
@@ -315,6 +321,7 @@ class DataObserver(NITLibrary.NITUserObserver):
                     self.roinorm_max = np.array(self.data['Servo.roinorm_max'][:self.profile_len**2]).reshape(
                         (self.profile_len, self.profile_len))
                     self.roinorm_amp = self.roinorm_max - self.roinorm_min
+                    
                     self.normalization_coeffs_tag = self._get_normalization_coeffs_tag()
 
                 
@@ -341,30 +348,49 @@ class DataObserver(NITLibrary.NITUserObserver):
             
             # # --------- T2: normalization + levels + angles/opd ----------
                             
-            roi_norm = np.clip((roi - self.roinorm_min) / self.roinorm_amp, 0, 1)
+            roi_norm = np.clip((roi - self.roinorm_min) / (self.roinorm_amp + 1e-8), 0, 1)
+                
+            ix_profile = self.profile_len//2 + int(self.data['IRCamera.profile_h_shift'][0])
+            iy_profile = self.profile_len//2 + int(self.data['IRCamera.profile_v_shift'][0])
+
+            mask = self.roinorm_max.astype(np.float32, copy=False) if self.roinorm_max is not None else None
             vprofile_norm, hprofile_norm = compute_profiles_local_f32(
                 roi_norm.astype(np.float32, copy=False),
-                int(self.iy), int(self.ix), int(self.iwid), int(self.profile_len), False)
+                int(iy_profile), int(ix_profile), int(self.iwid), int(self.profile_len), get_roi=False, mask=mask)
             vprofile, hprofile = compute_profiles_local_f32(
                 roi.astype(np.float32, copy=False),
-                int(self.iy), int(self.ix), int(self.iwid), int(self.profile_len), False)
-
-
-
+                int(iy_profile), int(ix_profile), int(self.iwid), int(self.profile_len), get_roi=False, mask=mask)
+            
             if compute_servo_output:
 
                 # Refresh pixel lists when states change
                 x_states = self.arr_xpix_states
                 y_states = self.arr_ypix_states
+            
                 if self.x_pixels_states is None or not np.array_equal(self.x_pixels_states, x_states):
                     self.x_pixels_states = np.copy(x_states)
-                    self.xpixels_list = utils.get_pixels_lists(x_states)
-                    self.xpixels_list_pos = utils.get_mean_pixels_positions(self.xpixels_list)
+                    try:
+                        xpixels_list = utils.get_pixels_lists(x_states)
+                        xpixels_list_pos = utils.get_mean_pixels_positions(xpixels_list)
+                    except Exception as e:
+                        log.error(f'error computing x pixels lists: {e}')
+                    else:
+                        self.xpixels_list = xpixels_list
+                        self.xpixels_list_pos = xpixels_list_pos
+                    
                     self.arr_hlev_pos[:3] = self.xpixels_list_pos
+                    
                 if self.y_pixels_states is None or not np.array_equal(self.y_pixels_states, y_states):
                     self.y_pixels_states = np.copy(y_states)
-                    self.ypixels_list = utils.get_pixels_lists(y_states)
-                    self.ypixels_list_pos = utils.get_mean_pixels_positions(self.ypixels_list)
+                    try:
+                        ypixels_list = utils.get_pixels_lists(y_states)
+                        ypixels_list_pos = utils.get_mean_pixels_positions(ypixels_list)
+                    except Exception as e:
+                        log.error(f'error computing y pixels lists: {e}')
+                    else:
+                        self.ypixels_list = ypixels_list
+                        self.ypixels_list_pos = ypixels_list_pos
+                        
                     self.arr_vlev_pos[:3] = self.ypixels_list_pos
 
                 # Normalized profiles (Numba kernels)
@@ -384,18 +410,21 @@ class DataObserver(NITLibrary.NITUserObserver):
             self.angles_ws[:2] = utils.compute_angles(self.hlevels_ws, self.arr_hellipse[:4],
                                                       self.arr_last_angles[:2])
             self.angles_ws[2:] = utils.compute_angles(self.vlevels_ws, self.arr_vellipse[:4],
-                                                      self.arr_last_angles[2:])
+                                                      self.arr_last_angles[2:])                
+                
             opds = utils.compute_opds(self.angles_ws)
             opds -= self.arr_mean_opd_offset[0]
             mean_opd = utils.mean(opds)
 
             # check for lost
-            if self.last_mean_opd is not None:
+            if self.last_mean_opd is not None and np.isfinite(mean_opd):
                 if abs(mean_opd - self.last_mean_opd) > config.IRCAM_LOST_THRESHOLD:
                     log.warning(f'potential lost detected: mean OPD jump from {self.last_mean_opd:.2f} nm to {mean_opd:.2f} nm')
                     self.data['Servo.is_lost'][0] = float(True)
-            self.last_mean_opd = float(mean_opd)
-                        
+
+            if np.isfinite(mean_opd):
+                self.last_mean_opd = float(mean_opd)
+                
             
             if compute_servo_output:
                 
@@ -424,7 +453,9 @@ class DataObserver(NITLibrary.NITUserObserver):
             
             
         except Exception as e:
-            log.error(f'error on new frame: {e}')
+            log.error(f'error on new frame: {e}, traceback: {traceback.format_exc()}')
+            
+            
 
         
     

servo/servo.py
@@ -130,8 +130,8 @@ class Servo(core.Worker):
                 ServoState.RUNNING, action=self._roi_mode),
             (ServoState.RUNNING, self.Event.FULL_FRAME_MODE): Transition(
                 ServoState.RUNNING, action=self._full_frame_mode),
-            (ServoState.TRACKING, self.Event.RESET_ZPD): Transition(
-                ServoState.TRACKING, action=self._reset_zpd),
+            (ServoState.RUNNING, self.Event.RESET_ZPD): Transition(
+                ServoState.RUNNING, action=self._reset_zpd),
             (ServoState.TRACKING, self.Event.WALK_TO_OPD): Transition(
                 ServoState.WALKING, action=self._walk_to_opd),
             (ServoState.WALKING, self.Event.STOP_WALKING): Transition(
@@ -166,7 +166,9 @@ class Servo(core.Worker):
 
     def get_pid_control(self, name,
                         out_min=config.PIEZO_V_MIN,
-                        out_max=config.PIEZO_V_MAX):
+                        out_max=config.PIEZO_V_MAX,
+                        deadband=0.0,
+                        kaw=5.0):
         
         dt = config.OPD_LOOP_TIME
 
@@ -176,7 +178,7 @@ class Servo(core.Worker):
             dt=dt,
             out_min=out_min,
             out_max=out_max,
-            deriv_filter_hz=1/dt/1000., kaw=5.0, deadband=0.0
+            deriv_filter_hz=1/dt/1000., kaw=kaw, deadband=deadband
         )
         
         return pid_control
@@ -241,8 +243,8 @@ class Servo(core.Worker):
                     last_update_time = time.time()
                         
                 opd = self.data['Tracker.opd_100'][0]
-                tip = self.data['Tracker.tip_30'][0]
-                tilt = self.data['Tracker.tilt_30'][0]
+                tip = self.data['Tracker.tip_0.1'][0]
+                tilt = self.data['Tracker.tilt_0.1'][0]
 
                 if not np.isnan(opd):
                     u_pid_opd = pid_opd_control.update(
@@ -453,6 +455,11 @@ class Servo(core.Worker):
 
         roinorm_min, roinorm_max = utils.get_roi_normalization_coeffs(
             np.array(rec_rois))
+
+        high_values = np.nanpercentile(roinorm_max, 99)
+        if  high_values > 0.75:
+            log.warning("High ROI normalization max value detected, may cause saturation: 99th percentile={:.3f}".format(high_values))
+            
         self.data['Servo.roinorm_min'][:profile_len**2] = roinorm_min.astype(config.FRAME_DTYPE).flatten()
         self.data['Servo.roinorm_max'][:profile_len**2] = roinorm_max.astype(config.FRAME_DTYPE).flatten()
 
@@ -555,9 +562,13 @@ class Servo(core.Worker):
         da2_max = da2_level_orig + max_v_diff
 
         pid_da1_control = self.get_pid_control(
-            'TRACK_DA1', out_min=da1_min, out_max=da1_max)
+            'TRACK_DA1', out_min=da1_min, out_max=da1_max,
+            deadband=self.data['params.PID_DA_DEADBAND'],
+            kaw=self.data['params.PID_DA_KAW'])
         pid_da2_control = self.get_pid_control(
-            'TRACK_DA2', out_min=da2_min, out_max=da2_max)
+            'TRACK_DA2', out_min=da2_min, out_max=da2_max,
+            deadband=self.data['params.PID_DA_DEADBAND'],
+            kaw=self.data['params.PID_DA_KAW'])
         
         # check if piezos are in a correct range
         if not (4 < self.data['DAQ.piezos_level'][0] < 6):
@@ -600,8 +611,8 @@ class Servo(core.Worker):
                     loop_startt = time.perf_counter()
 
                     opd = float(self.data['Tracker.opd_100'][0]) # nm
-                    tip = float(self.data['Tracker.tip_30'][0]) 
-                    tilt = float(self.data['Tracker.tilt_30'][0])
+                    tip = float(self.data['Tracker.tip_0.1'][0]) 
+                    tilt = float(self.data['Tracker.tilt_0.1'][0])
 
                     nexline_is_moving = NexlineState(int(self.data['Nexline.state'])).name == 'MOVING'
                     if time.perf_counter() - step_startt > 0.5: # wait for event dispatch
@@ -652,8 +663,12 @@ class Servo(core.Worker):
                         da2_min = da2_level_orig - max_v_diff
                         da2_max = da2_level_orig + max_v_diff
 
-                        pid_da1_control.update_config(out_min=da1_min, out_max=da1_max)
-                        pid_da2_control.update_config(out_min=da2_min, out_max=da2_max)
+                        pid_da1_control.update_config(out_min=da1_min, out_max=da1_max,
+                                                      deadband=self.data['params.PID_DA_DEADBAND'],
+                                                      kaw=self.data['params.PID_DA_KAW'])
+                        pid_da2_control.update_config(out_min=da2_min, out_max=da2_max,
+                                                      deadband=self.data['params.PID_DA_DEADBAND'],
+                                                      kaw=self.data['params.PID_DA_KAW'])
 
                         refresh_startt = time.perf_counter()
 

servo/tracker.py
@@ -105,11 +105,14 @@ class Record():
 
     def get_normalization_coeffs(self, width):
 
-        if self.position < 100:
+        if self.position < 2000:
             log.warning(f'Not enough data to compute normalization coefficients: only {self.position} frames recorded')
-            return None, None, None, None
+            return None, None
         
         ircube = self.ir_images[:self.position].copy()
+
+        # save ircube used for noramlization as an npy file for later analysis with a timestamp in the path
+        np.save(f'servo_tracker_normalization_ircube_{time.strftime("%Y%m%d-%H%M%S")}.npy', ircube)
         
         # recompute normalization maps
         # ix, iy = np.array(ircube.shape[1:])//2
@@ -143,7 +146,7 @@ class Tracker(core.Worker):
                 self.State.RUNNING, action=self._normalize),        
         }
         
-        self.frequencies = [int(ifreq) for ifreq in config.TRACKER_STATS_FREQUENCIES]
+        self.frequencies = [ifreq for ifreq in config.TRACKER_STATS_FREQUENCIES]
         if config.TRACKER_FREQUENCY not in self.frequencies:
             self.frequencies.append(config.TRACKER_FREQUENCY)
             
@@ -194,11 +197,13 @@ class Tracker(core.Worker):
         meantip = self.tip_buffer.mean_last(window_size)
         meantilt = self.tilt_buffer.mean_last(window_size)
         meantime = self.time_buffer.mean_last(window_size)
-        
-        self.data[f'Tracker.opd_{frequency}'][0] = float(meanopd)
-        self.data[f'Tracker.opd_std_{frequency}'][0] = float(self.opd_buffer.std_last(window_size))
-        self.data[f'Tracker.tip_{frequency}'][0] = float(meantip)
-        self.data[f'Tracker.tilt_{frequency}'][0] = float(meantilt)
+
+        frequency_str = f"{int(frequency)}" if frequency % 1 == 0 else f"{frequency:.1f}"
+
+        self.data[f'Tracker.opd_{frequency_str}'][0] = float(meanopd)
+        self.data[f'Tracker.opd_std_{frequency_str}'][0] = float(self.opd_buffer.std_last(window_size))
+        self.data[f'Tracker.tip_{frequency_str}'][0] = float(meantip)
+        self.data[f'Tracker.tilt_{frequency_str}'][0] = float(meantilt)
 
         self.last_opds[frequency].append(meanopd)
         self.last_times[frequency].append(meantime)
@@ -211,11 +216,12 @@ class Tracker(core.Worker):
                 velocity = self._last_velocity
                 log.warning(f'could not compute velocity @ {frequency}: invalid time difference: {_time}')
             if not np.isfinite(velocity):
-                velocity = self._last_velocity
                 log.warning(f'could not compute velocity @ {frequency}: non-finite value: {velocity}')
+                velocity = self._last_velocity
+
                 
             self._last_velocity = float(velocity)
-            self.data[f'Tracker.velocity_{frequency}'][0] = float(velocity)
+            self.data[f'Tracker.velocity_{frequency_str}'][0] = float(velocity)
                 
         else:
             velocity = np.nan
@@ -263,6 +269,9 @@ class Tracker(core.Worker):
         width = int(self.data['params.PROFILE_WIDTH'][0])
         
         roinorm_min, roinorm_max = self.record.get_normalization_coeffs(width)
+        if roinorm_min is None or roinorm_max is None:
+            log.warning('Could not compute normalization coefficients.')
+            return
         
         self.data['Servo.roinorm_min'][:self.profile_len**2] = roinorm_min.astype(config.FRAME_DTYPE).flatten()
         self.data['Servo.roinorm_max'][:self.profile_len**2] = roinorm_max.astype(config.FRAME_DTYPE).flatten()

servo/viewer/tabs/main.py
@@ -344,8 +344,8 @@ class MainTab:
                 ax.axhline(v, c='0.8' if v in (0,1) else '0.5')
             canvas.draw()
 
-        draw(self.fig_h, self.ax_h, self.canvas_h, horiz, 'h profile', hlevels, hpos)
-        draw(self.fig_v, self.ax_v, self.canvas_v, vert,  'v profile', vlevels, vpos)
+        draw(self.fig_h, self.ax_h, self.canvas_h, horiz, 'V profile', hlevels, hpos)
+        draw(self.fig_v, self.ax_v, self.canvas_v, vert,  'H profile', vlevels, vpos)
         self.hlevels_buf.appendleft(np.copy(hlevels))
         self.vlevels_buf.appendleft(np.copy(vlevels))
         draw_ellipse(self.fig_ellx, self.ax_ellx, self.canvas_ellx, self.hlevels_buf)